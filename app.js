ac3f2cdbb7f03432a3cee8b6a6f6cd4f289a844e
Debojeet Bhowmick <161986304+D3b0j33t@users.noreply.github.com>
2025-04-28 20:11:57 +0530

    api key updated

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Constants and Configuration
 // const API_KEY = "526O6sHKAhweBxQCqlf8ZJRrWwecVBioFH3nDEcD"; // Default API key for development
-const API_KEY = process.env.API_KEY;
+const API_KEY = "JzEMV8d3ajCCAj5UfctPpjYg4k6TCigTqfk5byNm" || "526O6sHKAhweBxQCqlf8ZJRrWwecVBioFH3nDEcD";
 const CACHE_DURATION = 15 * 60 * 1000; // 15 minutes
 const BREAKING_NEWS_REFRESH = 5 * 60 * 1000; // 5 minutes
 
